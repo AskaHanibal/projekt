dd53069ce77af89b7d6c1115c8f046927d8ecc50
Asia <aska.hanibal@wp.pl>
2018-09-13 19:11:49 +0200

    proba1

---
+++ hello.py
@@ -0,0 +1,27 @@
+print('hello \'world\'!')
+print ("3")
+print (3)
+
+number = 3
+number_str = "3"
+
+#type (number)
+#print (type (number_str))
+#print (type("number_str"))
+#print ("3.14")
+#print (0.1 + 0.1 + 0.1)
+#print (type (True))
+#print (type (None))
+
+
+number = number + 1
+hello = "Hello"
+world = " world"
+ex = " !"
+
+# konkatenacja stringów (łączenie)
+sign = hello + world + ex
+print (sign)
+
+name = input("Jak masz na imię?")
+print(hello + " " + name + ex)
---
+++ numbers.py
@@ -0,0 +1,7 @@
+number = 3
+number = number + 1
+print (number)
+print (number ** 2)
+print (number / 4)
+print (4 % 2)
+print (3 % 2)
